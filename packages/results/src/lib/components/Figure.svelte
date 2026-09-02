a4aa9c8eb438f4078055bf6cd9b8628a8811c229
David Longworth <djl@sent.com>
2026-09-02 16:51:59 +0100

    move share/cite button outside the chart

--- packages/results/src/lib/components/Figure.svelte
+++ packages/results/src/lib/components/Figure.svelte
@@ -62,17 +62,15 @@
 		<span class="sr-only">{caption}</span>
 
 		{#if href}
-			<p class="absolute right-2 bottom-2 flex gap-4 bg-black-150 dark:bg-black-500">
-				<a
-					{href}
-					onclick={open}
-					class="ml-auto flex items-center gap-1.5 px-3 py-2 hover:bg-black hover:text-white"
-					aria-label="Permalink: {block.demographic.name}"
-				>
-					Share or cite
-					<Icon src={IconLink} />
-				</a>
-			</p>
+			<a
+				{href}
+				onclick={open}
+				class="absolute right-0 top-full ml-auto flex items-center gap-1.5 px-3 py-2.5 hover:bg-black hover:text-white"
+				aria-label="Permalink: {block.demographic.name}"
+			>
+				Share or cite
+				<Icon src={IconLink} />
+			</a>
 		{/if}
 	</figcaption>
 </figure>
